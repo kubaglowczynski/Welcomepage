971276c3ed5fd60473cec3e8694e73a384370127
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2023-03-06 00:39:06 +0100

    some updates

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const nameVal = document.getElementById("name").value;
-const buttonSub = document.getElementById("submit");
+var nameVal = document.getElementById("name").value;
+var buttonSub = document.getElementById("submit");
+var inputEnter = document.getElementById("name");
 
 buttonSub.addEventListener("click", function(){
     var input = document.querySelector(".input-name");
@@ -16,5 +17,23 @@ buttonSub.addEventListener("click", function(){
         left: 0,
         behavior: 'smooth'
     });
+});
 
+inputEnter.addEventListener("keypress", function(event){
+    if(event.key === 'Enter'){
+        var input = document.querySelector(".input-name");
+    input.classList.add("input-name-scale");
+
+    setTimeout(() => { input.classList.add("input-name-display")}, 1000);
+    
+    var nameVal = document.getElementById("name").value;
+    var name = document.querySelector(".hello");
+    name.innerHTML = 'hello '+nameVal;
+
+    window.scrollTo({
+        top: name.offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    });
+    };
 });
